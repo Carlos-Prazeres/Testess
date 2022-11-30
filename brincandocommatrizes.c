297f7dd734b23661919a84d29593fb0c5884ba96
Carlos-Prazeres <116236117+Carlos-Prazeres@users.noreply.github.com>
2022-11-30 17:21:26 -0300

    Add files via upload

---
+++ brincandocommatrizes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void main ()
+{
+    int matriz [3][3];
+    int i, j, maior = 0, valor; 
+    double soma = 0;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf ("%d", &valor);
+            matriz[i][j] = valor;
+            soma = soma + matriz [i] [j];
+        }
+    }
+
+    maior = matriz[0][0];
+    printf ("%.2lf %d ", soma / 9, maior);
+    printf ("%d ", maior);
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (matriz[i][j] > maior)
+            {
+                maior = matriz [i] [j];
+            }
+        }
+    }
+
+    
+}
